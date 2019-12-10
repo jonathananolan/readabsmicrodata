6534dc591600ca49a099de7d6124f66855328d23
Jonathan Nolan <jonathan.a.nolan@gmail.com>
2019-12-11 01:15:44 +1100

    First working version

--- R/cleaning_functions.R
+++ R/cleaning_functions.R
@@ -35,7 +35,7 @@ inflate_survey <- function(data,to_year) {
 #' @examples
 #' @import dplyr
 #' @import purrr
-#' @importFrom stringr str_remove
+#' @importFrom stringr str_remove str_replace_all
 
 equivalise_survey<- function(data){
 
@@ -49,26 +49,41 @@ equivalise_survey<- function(data){
 
 
   if(first(data$survey) == "hes") {
-    data <- data %>% mutate(persons_under_15 = d1age+
-                                               d2age+
-                                               d3age+
-                                               d4age) }
+
+    converter <- c("One" = "1", "Two" = "2", "Three" = "3", "Four" = "3","Five" = 5, "Six" = 6, "Seven" = 7, "Eight" = 8, Not="0")
+    data <- data %>%
+                      mutate_at(.vars = vars(starts_with("children_number_")),
+                                .funs = list(~  str_replace_all(.,converter))) %>%
+                      mutate_at(.vars = vars(starts_with("children_number_")),
+                                .funs = list(~suppressWarnings(as.numeric(substr(.,1,1))))) %>%
+                      mutate_at(.vars = vars(starts_with("children_number_")),
+                                .funs = list(~if_else(is.na(.),0,.))) %>%
+                      mutate(persons_under_15 = children_number_1+
+                                                children_number_2+
+                                                children_number_3+
+                                                children_number_4,
+                             size_hh = str_replace_all(size_hh,converter),
+                             persons_15_plus = if_else(is.na(persons_15_plus),
+                                                       as.numeric(substr(size_hh,1,1))-persons_under_15,
+                                                       as.numeric(substr(persons_15_plus,1,1))))
+
+              }
 
 
-    data <- data %>% mutate(persons_15_plus  = as.numeric(substr(persons_15_plus,1,1)),
-                            persons_under_15 = as.numeric(substr(persons_under_15,1,1)),
-                            persons_under_15 = if_else(is.na(persons_under_15),0,persons_under_15),
-                            equiv_hh = if_else(is.na(equiv_hh),
-                                               (1 + 0.5 * (persons_15_plus - 1) + 0.3 * (persons_under_15)),
-                                               equiv_hh)) %>%
+  data %>% mutate(persons_15_plus  = suppressWarnings(as.numeric(substr(persons_15_plus,1,1))),
+                  persons_under_15 = suppressWarnings(as.numeric(substr(persons_under_15,1,1))),
+                  persons_under_15 = if_else(is.na(persons_under_15),0,persons_under_15),
+                  equiv_hh = if_else(is.na(equiv_hh),
+                                     (1 + 0.5 * (persons_15_plus - 1) + 0.3 * (persons_under_15)),
+                                     equiv_hh)) %>%
       mutate_at(.vars = var_names,
                 .funs = list(~ ./equiv_hh))
   }
 
-#' Equivalise all dollar value HES data
+#' Fix old vars
 #'
-#' This function allows you to inflate HES data. It will only inflate variables listed as 'inflate' in the filename
-#' @param data_directory A directory with all the required files to run this function.  /HESvariables contains two files: HESkeyvariablenames.csv which is a list of the variables you want in your dataset and filenames.csv contains the filenames of each DTA file in your data folder.
+#' Scale old versions of the HES and the SIH with outdated variables.
+#' @param data a tibble created by read_abs_microdata()
 #' @keywords HES
 #' @export
 #' @examples
@@ -152,9 +167,9 @@ data %>% mutate(age_approx = as.numeric(stri_extract_last_regex(age, "\\d{2}")))
 #'
 
 add_cohorts <- function(data){
-  cohorts <- read_csv( system.file("extdata", "cohorts.csv", package = "readabsmicrodata", mustWork = TRUE)) %>%
-    gather("refyear","cohort",-age_type,-age) %>%
-    mutate(refyear = as.numeric(refyear))
+  cohorts <- read.csv( system.file("extdata", paste0("cohorts_",first(data$survey),".csv"), package = "readabsmicrodata", mustWork = TRUE),check.names = FALSE,stringsAsFactors = FALSE) %>%
+    gather("year","cohort",-age_type,-age) %>%
+    mutate(year = as.numeric(year))
 
   cohort_5  <- cohorts %>% filter(age_type == "age_5") %>%  rename(cohort_5y  = cohort,age_5  = age) %>% select(-age_type)
   cohort_10 <- cohorts %>% filter(age_type == "age_10") %>% rename(cohort_10y = cohort,age_10 = age) %>% select(-age_type)
