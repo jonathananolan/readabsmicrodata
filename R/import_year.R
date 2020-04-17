7711e5e2c4c966b69e6078a51be0df55916a4b7b
Jonathan Nolan <jonathan.a.nolan@gmail.com>
2020-04-17 15:51:00 +1000

    Added person file

--- R/import_year.R
+++ R/import_year.R
@@ -4,13 +4,13 @@
 #'
 #' @keywords internal
 #' @import dplyr
-#' @importFrom haven read_sas
+#' @importFrom haven read_sas zap_labels
 #' @importFrom rio factorize
 #' @import grattandata
 
 
 
-# Function that can import one year of the SIH
+# Function that can import one year of the SIH/Hes
 import_year <- function(year_num,
                             filter_vars = NULL,
                             nrow = Inf,
@@ -72,7 +72,7 @@ import_year_filtered<- function(year_num,
   filtered_vars <- c(filtered_vars_upper,
                      filtered_vars_lower)
 
-  import_year(year_num,
+test<- import_year(year_num,
               filter_vars = filter_vars,
               nrow = Inf,
               survey = survey,
@@ -80,6 +80,8 @@ import_year_filtered<- function(year_num,
               file_names = file_names,
               grattandata = grattandata,
               data_dir = data_dir) %>%
+    mutate_at(vars(contains("agekidh")),
+              zap_labels) %>%
               factorize() %>%
     rename_at(vars(filtered_vars_lower), ~ new_vars_lower) %>%
     mutate(year =year_num )
